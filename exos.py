9a57863011dca94af005a7510d85b8198223c202
yoannklt <ykerlogot@gaming.tech>
2022-11-08 20:03:48 +0100

    exos fin 2ème séance

--- exos.py
+++ exos.py
@@ -17,39 +17,46 @@
 #premiereAssertionDeux = ((365 * 3) / (4 - (12 - 8)) * (rh) > r) = (1095 / 0 * 230 > 12000) #False
 #deuxiemeAssertionDeux = (e *s < r) = (10 * 1250 < 12000) = (12500 < 12000) #False
 #assertionFinaleDeux = premiereAssertionDeux == deuxiemeAssertionDeux #True
-x = 4
-y = 2
+#
 
-def add(x,y):
-    return x + y
+# def input():
+        # "oeoeoe"
+#Renvoie un caractère de type str au hasard
 
-def sub(x,y):
-    return x - y
+#Exercice:
+    #Faire un mini-jeu qui affiche un message lorsque input renvoie le bon caractère
+    #Le caractère doit être parametrable
 
-def multiply(x,y):
-    return x * y
-
-def divide(x,y):
-    return x / y
-
-def modulo(x,y):
-    return x % y
-
-
-def salaireNet(brut, coéfficient):
-    return brut * coéfficient
-
-def salaireParSeconde(salaireHoraire, heuresParJourOuvré, nbDeJoursOuvrésParAn):
-    return (salaireHoraire * heuresParJourOuvré * nbDeJoursOuvrésParAn) / (60 * 60 * 24 * 365)
-
-print(salaireParSeconde(12, 7, 235))
-
-def SalaireParSeconde(salaireHoraire, heuresParJourOuvré, jourOuvreParAn):
-    #Calculer mon salaire annuel
-    salaireAnnuel = salaireHoraire * heuresParJourOuvré * jourOuvreParAn
-    #Calculer le nombre de seconde dans une année
-    nbSecondeParAn = 365 * 24 * 60 * 60
-    #Je pose et retourne la division
-    return salaireAnnuel / nbSecondeParAn
+def miniJeu(resultLetter): 
+    if resultLetter != input("C'est quoi la lettre khoya ?"):
+        print("C'est perdu")
+        return miniJeu(resultLetter) + 1
+    else :
+        print("C'est gagné")
+        return 1
     
+print(miniJeu("e"))
+
+prenom = "Yoann"
+nom = "Kerlogot"
+identite = nom + " " + prenom  #retourne "Kerlogot Yoann"
+
+integerValue = 342 #Retourne 342
+stringIntegerValue = str(342) #Retourne "342"
+
+tableau = [0,10,15,5,1]
+#Pour récupérer 15 je prends dans tableau l'index 2
+
+print(tableau[2]) #Affiche 15
+
+#Exercice 1
+#Faire une fonction qui concatene 2 chaines de caractères, les séparants par une virgule
+
+#Exercice 2
+#Faire une fonction qui itere sur tous les index d'un tableau renvoyant une chaine de caractère avec l'ensemble des occurences d'un chiffre
+#Pour tableau = [0,1,1,1,0,1,1,0,1]
+#la fonction (tableau, 0) doit renvoyer "0, 4, 7" n'hésitez pas à implémenter la première fonction 
+
+#Exercice 3
+#Renvoyer/Afficher un message
 #FIN
