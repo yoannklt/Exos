c496b283d2b854a563a568dc54cdbeeffcefc4b0
yoannklt <ykerlogot@gaming.tech>
2022-11-09 10:02:08 +0100

    findIndex et fibonacci

--- exos.py
+++ exos.py
@@ -52,27 +52,52 @@ stringIntegerValue = str(342) #Retourne "342"
 #Exercice 1
 #Faire une fonction qui concatene 2 chaines de caractères, les séparants par une virgule
 
-def virgule(x, y):
-    motVirgule = x + ", " + y
-    print(motVirgule)
-    return
-
-virgule("Yoann", "Kerlogot")
+def concatWithComma(strA, strB):
+    stringifiedA = str(strA)
+    stringifiedB = str(strB)
+    return stringifiedA + ", " + stringifiedB
 
 #Exercice 2
 #Faire une fonction qui itere sur tous les index d'un tableau renvoyant une chaine de caractère avec l'ensemble des occurences d'un chiffre
 #Pour tableau = [0,1,1,1,0,1,1,0,1]
 #la fonction (tableau, 0) doit renvoyer "0, 4, 7" n'hésitez pas à implémenter la première fonction 
 tableau = [0,1,1,1,0,1,1,0,1]
+#Définir la fonction findIndex qui itère sur tableau, cherchant l'index
+#Des différentes occurences de x
+def findIndex(tableau, x) :
+    #Définir un index de départ
+    i = 0
+    #Je définie un boolean tel que firstTurn est true
+    firstTurn = True
+    #Définir chaineRetour telle qu'une chaine de caractère vide
+    chaineRetour = ''
+    #Tant que i est différent du nombre d'elt dans le tableau 
+    while (i != len(tableau)):
+        #Alors j'attribue a une variable la valeur de tableau à l'index i
+        selected = tableau[i]
+        #Si selected est égal à x et que firstTurn est true
+        if selected == x and firstTurn :
+            #Alors on assigne à chaineRetour le retour de str(i)
+            chaineRetour = str(i)
+            #Changer la valeur de firstTurn en False
+            firstTurn = False
+        #Sinon si selected est égale à x
+        elif selected == x :
+            #Alors j'assigne le retour de  concatWithComma tel que: concatWith Comma (chainerRetour, i) à chaineRetour
+            chaineRetour = concatWithComma(chaineRetour, i)
+        #J'incrémente i de 1
+        i = i + 1
+    #Retourner la chaine retour
+    return chaineRetour     
 
-def indexTableau(x):
-    print(tableau[x])
-    
-indexTableau(1)
+print(findIndex(tableau, 0))
 
 #Exercice 3
 #Renvoyer/Afficher un message
 
 print("un message")
+
+#Définir la fonction suiteFibonacci qui itère sur debutSuite, cherchant l'index
+#Des différentes occurences de x
 
 #FIN
